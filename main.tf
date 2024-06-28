5c48952218d2aa8458c9626d5bfb85149463b8f1
Vishalldwivedi <vdwivedi0109@gmail.com>
2024-06-28 20:02:02 +0530

    creted the Ec2 instance using terraform

--- main.tf
+++ main.tf
@@ -8,6 +8,7 @@ variable avail_zone{}
 variable env_prefix{}// for every resouce we are creatin make a prefix for the evn 
 // that it will be deployed to
 variable my_ip{} 
+variable instance_type{}
 
 resource "aws_vpc" "myapp-vpc" {
   cidr_block = var.vpc_cidr_block
@@ -104,8 +105,80 @@ resource "aws_security_group" "myapp-sg"{
       Name : "${var.env_prefix}-sg"
     }
 }
+/*
+data "aws_ami" "latest-amazon-linux-image"{
+  most_recent = true
+  owners = ["amazon"]
+  filter {
+    name = "name"// 
+    values = ["amzn2-ami-kernel-*-x86_64-gp2"]// for name what values we want to filter on . 
+  }// this will give us all the images which matches this regular expression . 
+  //and give me the most recent of those . 
+  filter{
+    name = "virtulization-type"
+    values = ["hvm"]
+  }// this way we dont need to worry about hardcoding the value of what image i need 
+}
+*/
+data "aws_ami" "latest-amazon-linux-image" {
+  most_recent = true
+  owners = ["amazon"]
+  filter {
+    name = "name"
+    values = ["amzn2-ami-kernel-*-x86_64-gp2"]
+  }
+filter {
+  name = "virtualization-type"
+  values = ["hvm"]
+  }
+}
+/*
+
+output "aws_ami_id"{// we can see the output of aws_ami // use terraform plan= to see the image id 
+  value = data.aws_ami.latest-amazon-linux-image.id  
+}
 
 resource "aws_instance" "myapp-server"{
-  ami = //is the image whcih the ec2 server will be based on or OS image
-  
+  ami = data.aws_ami.latest-amazon-linux-image.id//is the image which the ec2 server will be based on or OS image
+
+  instance_type = var.instance_type
+
+  // first we have subnetid 
+  // we want our ec2 endup in our subnet 
+  subnet_id = aws_subnet.myapp-subnet-1.id
+
+  //vpc security group ids
+   vpc_security_group_ids = [aws_security_group.myapp-sg.id]
+   availability_zone = var.avail_zone//taking from the variable already defined
+   associate_public_ip_address = true // we need to access this instance from the browser 
+   //as well as ssh into it 
+   // so we need a public ip address of the server . 
+   key_name = "terraform-server-key-pair"
+
+   tags = {// naming our resouces for better understand of whole does this belong
+      Name : "${var.env_prefix}-server"
+    }
 }
+*/
+
+output "aws_ami_id" {
+  value = data.aws_ami.latest-amazon-linux-image.id 
+  }
+  
+  resource "aws_instance" "myapp-server" {
+  ami = data.aws_ami.latest-amazon-linux-image.id
+  instance_type = var.instance_type
+  subnet_id=aws_subnet.myapp-subnet-1.id
+  vpc_security_group_ids = [aws_security_group.myapp-sg.id]
+  availability_zone = var.avail_zone
+  
+  associate_public_ip_address = true
+  
+  key_name = "terraform-server-key-pair"
+  
+  tags = {// naming our resouces for better understand of whole does this belong
+      Name : "${var.env_prefix}-server"
+    }
+}
+
+
